1393850119df1ed4b85b9a424a3bbf03587f8e60
Taehyon Lee <leeth7830@gmail.com>
2019-09-16 11:02:41 -0700

    finish test

--- script/test.py
+++ script/test.py
@@ -14,9 +14,21 @@ table = dynamodb.Table('location-audience-2019-08-17')
 location_hashs = ["ac2959cc7ac7466bd91b08e58e5490d2", "ac0e4be8494c4c34a4c8c4e1ca47253c", "aeafeac6a78c548227f37bdd32cfb516", "eb54b7e048c7353be69954ea451d0276", "3c8da0f4e53ecd2e20fb620962bda2f3", "ea2c659d3b5fead1b7ced561722e9216"]
 segment_ids = [[61],[755],[77],[61,755,77]]
 def calculateAdomniScore(table, locationHash, segment_ids):
-    response = table.query(KeyConditionExpression=Key('hash_key').eq(locationHash) & Key('range_key').eq(61))
-    print(response)
+    for segment_id in segment_ids:
+        string_id = map(str, segment_id)
+        list_string = ', '.join(string_id)
+        print("For segment id: " + list_string)
+        for id in segment_id:
+            response = table.query(KeyConditionExpression=Key('locationHash').eq(locationHash) & Key('audienceSegmentId').eq(id))
+            if len(response['Items']) > 0:
+                count = str(response['Items'][0]['count'])
+                print("Count: " + count)
+            else:
+                print("Count: 0")
+                return 0
     return 1
 
 for location_hash in location_hashs:
+    print("For locatin hash: " + location_hash)
     calculateAdomniScore(table, location_hash, segment_ids)
+    print("-----------------------------------------------------------------------------------")
