f60d3480e6641fd79b8900287348fda23f403729
Kei Fukutani <kifinewera@gmail.com>
2019-10-20 16:41:49 -0700

    modified and tested algo_test.py

--- algo_test.py
+++ algo_test.py
@@ -1,5 +1,9 @@
-
-
+#####################################################################
+# Calculate Adomni Score given the billboard_id and the audience_ids. 
+# Version: 1.0.0
+# Author: Tae, Tuo, and Kei
+# Note  : This needs some other programs for pre-calculation. 
+#####################################################################
 
 #precalculate values at night time for maximum?
 
@@ -28,6 +32,7 @@ athena = boto3.client('athena')
 s3 = boto3.resource('s3')
 
 
+# Get the count of mobile devices for the billboard_id and the placeiqid. 
 def get_count(billboard_id, placeiqid):
     audience_data = pd.DataFrame(pd.read_csv('data/counts_for_each_audience/' + placeiqid + '.csv'))
     response = audience_data[audience_data['billboard_id'] == billboard_id]['my_count'].values    
@@ -35,6 +40,7 @@ def get_count(billboard_id, placeiqid):
     return response
 
 
+# Make count map that maps audience_id to count of mobile devices. 
 def get_count_map(billboard_id, audience_ids):
     # aud_seg_to_count (Key: audience_segment_id, Value: count)
     aud_seg_to_count = {}
@@ -49,14 +55,17 @@ def get_count_map(billboard_id, audience_ids):
     return aud_seg_to_count
 
 
+# Access the precalculated values in the above database to compare the count of mobile devices with the precalculated statistic for that audience_id. 
+# For now, use maximum count to normalize the count. 
 def get_max_count(audience_id):
-    # Take as input the result_max.csv here.
+    
     max_counts = pd.read_csv('data/result_max.csv')
     max_count = max_counts[max_counts['id'] == audience_id]['max']
 
     return max_count.values[0]
 
 
+# Get the normalized count for each audience_id. 
 def get_normalized_count(aud_seg_to_count, audience_ids):
     for audience_id in audience_ids:
         count = aud_seg_to_count[audience_id]
@@ -68,34 +77,6 @@ def get_normalized_count(aud_seg_to_count, audience_ids):
     return aud_seg_to_count
 
 
-#precalculate values at night time for maximum?
-#using dynamo or athena data for each segmentId
-#append the result into a database 
-
-#calculate score
-def calculate_score(billboard_id, audience_ids):
-    score = 0
-
-
-    #access the precalculated values in the above database to compare the count of locationHash with the precalculated statistic for that audienceId
-
-    # aud_seg_to_count (Key: audience_id, Value: count)
-    aud_seg_to_count = get_count_map(billboard_id, audience_ids)
-
-    # aud_seg_to_normalized (Key: audience_segment_id, Value: normalized count)
-    aud_seg_to_normalized = get_normalized_count(aud_seg_to_count, audience_ids)
-
-    print('aud_seg_to_normalized: \n{}'.format(aud_seg_to_normalized))
-
-    #score and save the array of all the result for each audienceId and calulate the average
-
-
-
-        
-
-    #return the result 
-    return aud_seg_to_normalized
-
 #segId_by_normalizedScore should be a map like {("b6e71c034fa5e34b5d8a9199208d53cb", 76): 0.3, ("b6e71c034fa5e34b5d8a9199208d53cb", 176): 0.85, ("b6e71c034fa5e34b5d8a9199208d53cb", 85): 0.4}
 def getIntegratedAdomniScore(segId_by_normalizedScore):
     integratedAdomniScore = 0
@@ -104,15 +85,75 @@ def getIntegratedAdomniScore(segId_by_normalizedScore):
     return integratedAdomniScore/len(segId_by_normalizedScore)
 
 
-segId_by_normalizedScore = {}
+# Calculate Adomni Score.  
+def calculate_score(billboard_id, audience_ids):
+    adomni_score = 0
+
+    # aud_seg_to_count (Key: audience_id, Value: count)
+    aud_seg_to_count = get_count_map(billboard_id, audience_ids)
+
+    # aud_seg_to_normalized (Key: audience_segment_id, Value: normalized count)
+    aud_seg_to_normalized = get_normalized_count(aud_seg_to_count, audience_ids)
+    print('aud_seg_to_normalized: \n{}'.format(aud_seg_to_normalized))
+
+
+    # score and save the array of all the result for each audienceId and calulate the average
+    segId_by_normalizedScore = {}
+    for audience_segment_id in aud_seg_to_normalized:
+        segId_by_normalizedScore[(billboard_id, audience_segment_id)] = aud_seg_to_normalized[audience_segment_id]
+
+    adomni_score = getIntegratedAdomniScore(segId_by_normalizedScore)
+        
+    return adomni_score
+
+
+
+
+#####################
+# Start here. 
+#####################
+
+# test case
 billboard_id = 'dbb561c792f78028f262e88ce95f857c'
 audience_ids = ['748', '738']
-aud_seg_to_normalized = calculate_score(billboard_id, audience_ids)
 
-for audience_segment_id in aud_seg_to_normalized:
-    segId_by_normalizedScore[(billboard_id, audience_segment_id)] = aud_seg_to_normalized[audience_segment_id]
+#precalculate values at night time for maximum?
+#using dynamo or athena data for each segmentId
+#append the result into a database
 
-print(getIntegratedAdomniScore(segId_by_normalizedScore))
+
+adomni_score = calculate_score(billboard_id, audience_ids)
+
+print(adomni_score)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
