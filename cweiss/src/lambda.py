91869375bcc1a3153df711770c0ae1489718d098
Chris Weiss <bitblit@gmail.com>
2019-11-06 10:59:06 -0800

    Checkpoint - adding test stuff

--- cweiss/src/lambda.py
+++ cweiss/src/lambda.py
@@ -3,7 +3,6 @@
 """
 
 import json
-import boto3
 
 # Set all these variables when you upload (
 bucket_name = 'your-bucket'
@@ -13,7 +12,7 @@ converted_prefix='ready'
 # While a file shows up in the unconverted bucket, run our processor
 def start_usf_processor(event, context):
 
-    print ("Processing usf handler")
+    print ("Processing usf handler for " + json.dumps(event))
 
     try:
         if (event!=None and event.has_key('Records') and
@@ -36,5 +35,14 @@ def start_usf_processor(event, context):
             return {'status' : 'ignored', 'message':'Invalid input'}
 
     except Exception as exception:
+        print("Failed to execute : " + str(exception))
         return {'status' : 'error',
-                'message' : exception.message}
+                'message' : str(exception)}
+
+
+# For running tests
+with open ("test-s3-put.json", "r") as myfile:
+    event = json.loads(myfile.readlines())
+
+context = {}
+start_usf_processor(event, context)
